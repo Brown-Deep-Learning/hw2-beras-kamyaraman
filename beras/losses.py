f244b247c271c807b28d0ba219cf480fad48ac8e
kamyaraman <kamya@raman-home.com>
2025-02-11 15:53:41 -0500

    mse fr this time

--- beras/losses.py
+++ beras/losses.py
@@ -17,11 +17,11 @@ class Loss(Diffable):
 class MeanSquaredError(Loss):
     def forward(self, y_pred: Tensor, y_true: Tensor) -> Tensor:
         means = np.mean((y_pred - y_true) ** 2, axis = -1)
-        total = np.mean(means)
+        total = np.mean(means)/self.inputs[0].shape[0]
         return Tensor(total)
 
     def get_input_gradients(self) -> list[Tensor]:
-        y_pred_gradient = (2 * (self.inputs[0] - self.inputs[1]))/self.inputs[0].shape[0]
+        y_pred_gradient = (2 * (self.inputs[0] - self.inputs[1])) / self.inputs[0].shape[0]
         y_true_gradient = np.zeros(self.inputs[1].shape) 
         return [Tensor(y_pred_gradient), Tensor(y_true_gradient)]
 
