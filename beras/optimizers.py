554bcc494b1a7c2d7fe55e72781d0a095bbbb43c
kamyaraman <kamya@raman-home.com>
2025-02-12 14:41:40 -0500

    optimizers

--- beras/optimizers.py
+++ beras/optimizers.py
@@ -5,8 +5,7 @@ class BasicOptimizer:
     def __init__(self, learning_rate):
         self.learning_rate = learning_rate
     def apply_gradients(self, trainable_params, grads):
-        return NotImplementedError
-
+        trainable_params.assign(trainable_params - self.learning_rate * grads)
 
 class RMSProp:
     def __init__(self, learning_rate, beta=0.9, epsilon=1e-6):
@@ -16,7 +15,10 @@ class RMSProp:
         self.v = defaultdict(lambda: 0)
 
     def apply_gradients(self, trainable_params, grads):
-        return NotImplementedError
+        self.v = self.beta * self.v + (1 - self.beta) * grads[i] ** 2
+        trainable_params.assign(trainable_params - self.learning_rate * grads / (np.sqrt(self.v) + self.epsilon))
+
+
 
 
 class Adam:
@@ -35,4 +37,9 @@ class Adam:
         self.t = 0                              # Time counter
 
     def apply_gradients(self, trainable_params, grads):
-        return NotImplementedError
+        self.t += 1
+        self.m = self.beta_1 * self.m + (1 - self.beta_1)*(grads)
+        self.v = self.beta_2 * self.v + (1 - self.beta_2)*(grads**2)
+        m_hat = self.m / (1 - self.beta_1**self.t)
+        v_hat = self.v / (1 - self.beta_2**self.t)
+        trainable_params.assign(trainable_params - self.learning_rate * m_hat / (np.sqrt(v_hat) + self.epsilon))
