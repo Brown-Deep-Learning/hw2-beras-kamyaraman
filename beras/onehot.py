6d8785ea843ddedd683bbcea9f20d0c338bcf7c2
kamyaraman <kamya@raman-home.com>
2025-02-12 09:19:12 -0500

    one hot

--- beras/onehot.py
+++ beras/onehot.py
@@ -4,11 +4,13 @@ from beras.core import Callable
 
 
 class OneHotEncoder(Callable):
+
+    def __init__(self):
+        self.one_hot = None
     """
     One-Hot Encodes labels. First takes in a candidate set to figure out what elements it
     needs to consider, and then one-hot encodes subsequent input datasets in the
     forward pass.
-
     SIMPLIFICATIONS:
      - Implementation assumes that entries are individual elements.
      - Forward will call fit if it hasn't been done yet; most implementations will just error.
@@ -23,10 +25,16 @@ class OneHotEncoder(Callable):
         :param data: 1D array containing labels.
             For example, data = [0, 1, 3, 3, 1, 9, ...]
         """
-        return NotImplementedError
+        unique = np.unique(data)
+        eye = np.eye(len(unique))
+        self.one_hot = {}
+        for i in range(len(unique)):
+            self.one_hot[unique[i]] = eye[i]
 
     def forward(self, data):
-        return NotImplementedError
+        return np.array([self.one_hot[d] for d in data])
 
     def inverse(self, data):
-        return NotImplementedError
+        inverse = {(v,k) for k,v in self.one_hot.items()}
+        return np.array([inverse[d] for d in data])
+    
